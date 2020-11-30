f16ba656db989ae07bb24ff2de308345c97c0f95
Chris Lee <chrislee910320@gmail.com>
2020-11-30 14:45:31 -0700

    commit for deployment

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import { Card, CardContent, Typography, Grid, StylesProvider } from '@material-ui/core'
-import CountUp from 'react-countup';
-import cx from 'classnames'
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
+import cx from "classnames";
 
-import styles from './Cards.module.css'
+import styles from "./Cards.module.css";
 
 const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
-
   if (!confirmed) {
-    return 'Loading...';
+    return "Loading...";
   }
   return (
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.infected)}
+        >
           <CardContent>
-            <Typography color="textSecondary" gutterBottom>Infected</Typography>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
             <Typography variant="h5">
               <CountUp
                 start={0}
@@ -24,13 +31,25 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 separator=","
               />
             </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of active cases of Covid-19</Typography>
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Number of active cases of Covid-19
+            </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.recovered)}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.recovered)}
+        >
           <CardContent>
-            <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+            <Typography color="textSecondary" gutterBottom>
+              Recovered
+            </Typography>
             <Typography variant="h5">
               <CountUp
                 start={0}
@@ -39,15 +58,25 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 separator=","
               />
             </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of recoveries from Covid-19</Typography>
-
-
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Number of recoveries from Covid-19
+            </Typography>
           </CardContent>
         </Grid>
-        <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)}>
+        <Grid
+          item
+          component={Card}
+          xs={12}
+          md={3}
+          className={cx(styles.card, styles.deaths)}
+        >
           <CardContent>
-            <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
             <Typography variant="h5">
               <CountUp
                 start={0}
@@ -56,15 +85,17 @@ const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
                 separator=","
               />
             </Typography>
-            <Typography color="textSecondary">{new Date(lastUpdate).toDateString()}</Typography>
-            <Typography variant="body2">Number of death caused by Covid-19</Typography>
-
-
+            <Typography color="textSecondary">
+              {new Date(lastUpdate).toDateString()}
+            </Typography>
+            <Typography variant="body2">
+              Number of death caused by Covid-19
+            </Typography>
           </CardContent>
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Cards
+export default Cards;
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -41,28 +41,28 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     />
   ) : null;
 
-  const lineChart = dailyData[0] ? (
-    <Line
-      data={{
-        labels: dailyData.map(({ date }) => date),
-        datasets: [
-          {
-            data: dailyData.map((data) => data.confirmed),
-            label: "Infected",
-            borderColor: "#3333ff",
-            fill: true,
-          },
-          {
-            data: dailyData.map((data) => data.deaths),
-            label: "Deaths",
-            borderColor: "red",
-            backgroundColor: "rgba(255, 0, 0, 0.5)",
-            fill: true,
-          },
-        ],
-      }}
-    />
-  ) : null;
+  // const lineChart = dailyData[0] ? (
+  //   <Line
+  //     data={{
+  //       labels: dailyData.map(({ date }) => date),
+  //       datasets: [
+  //         {
+  //           data: dailyData.map((data) => data.confirmed),
+  //           label: "Infected",
+  //           borderColor: "#3333ff",
+  //           fill: true,
+  //         },
+  //         {
+  //           data: dailyData.map((data) => data.deaths),
+  //           label: "Deaths",
+  //           borderColor: "red",
+  //           backgroundColor: "rgba(255, 0, 0, 0.5)",
+  //           fill: true,
+  //         },
+  //       ],
+  //     }}
+  //   />
+  // ) : null;
 
   return (
     <div className={styles.container}>
